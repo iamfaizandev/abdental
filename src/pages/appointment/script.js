3f6959305ea638ac54c3ff29613a347c216850a7
iamfaizandev <mdfaizan7563@gmail.com>
2024-05-05 20:43:50 +0530

    updates

--- src/pages/appointment/script.js
+++ src/pages/appointment/script.js
@@ -1,19 +1,32 @@
 function sendToWhatsapp() {
-  // let number = +917549692029;
-  let name = document.getElementById("flName").value;
-  let email = document.getElementById("UserEmail").value;
-  let message = document.getElementById("Message").value;
+  let name = document.getElementById("username").value.trim();
+  let email = document.getElementById("userEmail").value.trim();
+  let message = document.getElementById("message").value.trim();
+  let date = document.getElementById("date").value.trim();
+  let phoneno = document.getElementById("userphone").value.trim();
+  let time = document.getElementById("time").value.trim();
+
+  if (!name || !email || !message || !date || !phoneno || !time) {
+    alert("Please fill in all the fields.");
+    return;
+  }
 
   var url =
-    "https://wa.me/917549692029?text=" +
-    "Full Name :" +
-    name +
-    "%0a" +
-    "Email:" +
-    email +
-    "%0a" +
-    "Message:" +
-    message;
+    "https://wa.me/917563092029?text=" +
+    encodeURIComponent(
+      "Full Name: " +
+        name +
+        "\nDate: " +
+        date +
+        "\nPhoneNo: " +
+        phoneno +
+        "\nTime: " +
+        time +
+        "\nEmail: " +
+        email +
+        "\nMessage: " +
+        message
+    );
 
   console.log(url);
   window.open(url, "_blank").focus();
